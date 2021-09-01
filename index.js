ea501ac141abde39fc736afb41ccfbf49a1593ba
Dovilė <dovile.keparute@gmail.com>
2021-09-02 00:08:47 +0300

    Get planet name

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 /*
 You get an array of numbers, return the sum of all of the positives ones.
-
 Example [1,-4,7,12] => 1 + 7 + 12 = 20
-
 Note: if there is nothing to sum, the sum is default to 0.
 */
 
@@ -16,3 +14,41 @@ function positiveSum(arr) {
     return sum;
 }
 console.log(positiveSum([1, 2, 3, 4, 5]));
+
+/*
+The function is not returning the correct values. Can you figure out why?
+Example (Input --> Output ):
+3 --> "Earth"
+*/
+
+function getPlanetName(id) {
+    let name;
+    switch (id) {
+        case 1:
+            name = 'Mercury';
+            break;
+        case 2:
+            name = 'Venus';
+            break;
+        case 3:
+            name = 'Earth';
+            break;
+        case 4:
+            name = 'Mars';
+            break;
+        case 5:
+            name = 'Jupiter';
+            break;
+        case 6:
+            name = 'Saturn';
+            break;
+        case 7:
+            name = 'Uranus';
+            break;
+        case 8:
+            name = 'Neptune';
+            break;
+    }
+    return name;
+}
+console.log(getPlanetName(2));
