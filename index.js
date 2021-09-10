68e52ea2e4b87a26c0a182a7538da8ef50cd4675
Dovilė <dovile.keparute@gmail.com>
2021-09-10 11:46:18 +0300

    Codewars

--- index.js
+++ index.js
@@ -61,3 +61,70 @@ function check(a, x) {
     return a.includes(x)
 }
 console.log(check([66, 101], 66));
+
+/*
+Given a year, return the century it is in. Example: 1705 --> 18 */
+function century(year) {
+    return Math.ceil(year / 100);
+}
+/*
+In this simple assignment you are given a number and have to make it negative. Example: makeNegative(1); // return -1 */
+function makeNegative(num) {
+    return num <= 0 ? num : -num;
+}
+/*
+Write a function, gooseFilter / goose-filter / goose_filter / GooseFilter, that takes an array of strings as an argument and returns a filtered array containing the same elements but with the 'geese' removed.
+
+The geese are any strings in the following array, which is pre-populated in your solution:
+
+geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+For example, if this array were passed as an argument:
+
+["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+Your function would return the following array:
+
+["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+*/
+function gooseFilter(birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return gooseFilter.pop(geese);
+    // or other solution:
+};
+
+function gooseFilter(birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+
+    // sarasas kuriame kaupsime visas leistinas reiksmes
+    const rez = [];
+
+    /*
+    einame per gauta sarasa (birds)
+        paimame birds sarase esanti nari (bird)
+        reikia palyginti ar bird yra geese sarase
+        reikia eiti per geese sarasa
+            paimti geese nari
+            jeigu, geese narys sutampa su bird reiksme, tai ji yra neleistina / neitrakiama i rez sarasa
+            priesingu atveju - bird itraukiame i rez sarasa
+    */
+
+    for (let b = 0; b < birds.length; b++) {
+        const bird = birds[b];
+
+        let yraSarase = false;
+        for (let g = 0; g < geese.length; g++) {
+            const goose = geese[g];
+            if (goose === bird) {
+                yraSarase = true;
+                break;
+            }
+        }
+
+        if (!yraSarase) {
+            rez.push(bird);
+        }
+    }
+
+    return rez;
+}
+
