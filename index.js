01fef2d92c71facf99fc25184077dfdf29832319
Dovilė <dovile.keparute@gmail.com>
2021-09-17 20:45:11 +0300

    Codewars

--- index.js
+++ index.js
@@ -280,3 +280,16 @@ var summation = function (num) {
     return sum
   }
   console.log(summation(1));
+
+  /*
+  In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+Example:
+
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+*/
+function highAndLow(numbers){
+    return Math.max(...numbers.split(' ')) + ' ' + Math.min(...numbers.split(' '));
+  }
