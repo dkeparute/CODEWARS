d770084a40bfcdc0bab337e4810393d4d4b04844
Dovilė <dovile.keparute@gmail.com>
2021-09-18 13:03:56 +0300

    update

--- index.js
+++ index.js
@@ -293,3 +293,19 @@ highAndLow("1 9 3 4 -5"); // return "9 -5"
 function highAndLow(numbers){
     return Math.max(...numbers.split(' ')) + ' ' + Math.min(...numbers.split(' '));
   }
+
+  /*
+  Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+  */
+  function findShort(s){
+    var arr = s.split(' ');
+    var smallest = arr[0];
+    for (var i = 0; i < arr.length; i++) {
+      if(arr[i].length < smallest.length){
+        smallest = arr[i];
+      }
+    }
+    return smallest.length;
+  }
