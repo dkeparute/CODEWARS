a435995e887081728e5124e2b0c7a9b2c19fb763
Dovilė <dovile.keparute@gmail.com>
2021-09-20 22:33:51 +0300

    updatwe

--- index.js
+++ index.js
@@ -419,3 +419,37 @@ function pipeFix(numbers){
     }
       
  console.log(pipeFix([1,2,3,5,6,8,9]));
+
+ /*
+ Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side of the DNA (string, except for Haskell); you need to get the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+Example: (input: output)
+
+DNAStrand ("ATTGC") // return "TAACG"
+DNAStrand ("GTAT") // return "CATA" 
+*/
+function DNAStrand(dna){
+    dna = dna.toUpperCase();
+    let complement = '';
+    for(let i = 0; i < dna.length; i++){
+    if(dna[i] === 'T'){
+    complement += 'A';
+    }
+    if(dna[i] === 'A'){
+    complement += 'T';
+    }
+    if(dna[i] === 'C'){
+    complement += 'G';
+    }
+    if(dna[i] === 'G'){
+    complement += 'C';
+    }
+    }
+    return complement;
+    }
